5febb245ca445f68813a19cf85c25d5df0db4751
Anthony <maiorania@gmail.com>
2024-02-18 05:38:47 -0600

    Automate app integration
    Adds services which can be called by Automate for mounting and unmounting shares

---
+++ app/src/main/java/ru/nsu/bobrofon/easysshfs/UnmountReceiver.kt
@@ -0,0 +1,15 @@
+package ru.nsu.bobrofon.easysshfs
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.topjohnwu.superuser.Shell
+import ru.nsu.bobrofon.easysshfs.mountpointlist.MountPointsList
+
+class UnmountReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (context == null) return
+        val mountPointsList = MountPointsList.instance(context)
+        mountPointsList.umount(Shell.getShell())
+    }
+}
